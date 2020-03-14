f7216962287d625d168720f723cbc46218e99c26
Bianca Villar <bianca.villar@vtex.com.br>
2020-03-14 20:13:38 -0300

    Add upload page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,22 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import './App.css'
-import { sayHello } from './utils/api'
+import PrivateRoute from './PrivateRoute'
+import { AuthProvider } from './Auth'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
-import { AuthProvider } from './Auth'
-import PrivateRoute from './PrivateRoute'
+import Upload from './pages/Upload'
+import './App.css'
 
 class App extends Component {
-  handleButtonClick = () => {
-    sayHello()
-      .then(console.log)
-      .catch(console.log)
-  }
-
   render() {
     return (
       <AuthProvider>
         <Router>
           <div>
-            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/" component={Home} />
+            <PrivateRoute exact path="/upload" component={Upload} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
           </div>
