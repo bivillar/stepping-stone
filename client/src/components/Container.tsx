01f19e9ad68c46a2b6376b452df09cdedb33df74
Bianca Villar <bianca.villar@vtex.com.br>
2020-03-15 14:31:51 -0300

    Change colors

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -10,30 +10,50 @@ interface Props {
   children?: ReactNode
 }
 
-const Container: FC<Props> = ({ history, children }) => {
+const Container: FC<Props> = ({ page, history, children }) => {
   const { currentUser } = useContext(AuthContext)
 
   const goTo = (location: string) => {
     history.push(location)
   }
 
+  const classNames = {
+    home: page.toLowerCase() === 'home' ? 'b' : '',
+    upload: page.toLowerCase() === 'upload' ? 'b' : '',
+    login: page.toLowerCase() === 'login' || page === 'signup' ? 'b' : '',
+  }
+
   return (
     <>
-      <BootstrapNavbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <BootstrapNavbar collapseOnSelect expand="lg" bg="light" variant="dark">
         <BootstrapNavbar.Brand onClick={() => goTo('/')}>
           Stepping Stone
         </BootstrapNavbar.Brand>
         <BootstrapNavbar.Toggle aria-controls="responsive-navbar-nav" />
         <BootstrapNavbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link onClick={() => goTo('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => goTo('/upload')}>Upload</Nav.Link>
+            <Nav.Link className={classNames.home} onClick={() => goTo('/')}>
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className={classNames.upload}
+              onClick={() => goTo('/upload')}>
+              Upload
+            </Nav.Link>
           </Nav>
           <Nav>
             {currentUser ? (
-              <Nav.Link onClick={() => app.auth().signOut()}>Sign out</Nav.Link>
+              <Nav.Link
+                className={classNames.login}
+                onClick={() => app.auth().signOut()}>
+                Sign out
+              </Nav.Link>
             ) : (
-              <Nav.Link onClick={() => goTo('/login')}>Sign in</Nav.Link>
+              <Nav.Link
+                className={classNames.login}
+                onClick={() => goTo('/login')}>
+                Sign in
+              </Nav.Link>
             )}
           </Nav>
         </BootstrapNavbar.Collapse>
