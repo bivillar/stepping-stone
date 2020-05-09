fc4c7b1815eb831a11e4b577a165ad62d9bd7c57
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-09 20:18:02 -0300

    Add title fixed after scrolling

--- client/src/pages/Home/Title.tsx
+++ client/src/pages/Home/Title.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import Logo from '../../components/Logo'
 import GradYear from '../../components/charts/GradYear'
 import useVisible from '../../components/hooks/useVisible'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
-const Title: FC<Props> = ({}) => {
+const Title: FC<Props> = ({ setShowSmallTitle, showSmallTile }) => {
   const [isVisible, setRef] = useVisible()
+  const [fontSize, setFontSize] = useState<number>(90)
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    if (currPos.y > -230) {
+      setFontSize(90 + (currPos.y - (currPos.y % 5)) / 5)
+      if (showSmallTile) setShowSmallTitle(false)
+    } else if (currPos.y < -230 && !showSmallTile) {
+      setShowSmallTitle(true)
+    }
+  })
 
   return (
     <div
@@ -15,13 +26,16 @@ const Title: FC<Props> = ({}) => {
         className={`self-start fade-in-section ${
           isVisible ? 'is-visible' : ''
         }`}>
-        <Logo />
+        {!showSmallTile && <Logo fontSize={fontSize} />}
       </div>
       <div className="self-end">{isVisible && <GradYear />}</div>
     </div>
   )
 }
 
-interface Props {}
+interface Props {
+  showSmallTile: boolean
+  setShowSmallTitle: (show: boolean) => void
+}
 
 export default Title
