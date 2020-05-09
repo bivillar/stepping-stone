2095fde691ce915d8e330f10695fae82316bc220
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-09 17:39:20 -0300

    Got the colors right

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const Logo: FC = () => (
+  <div className="logo">
+    <div className="stepping">STEPPING</div>
+    <div className="stone">STONE</div>
+  </div>
+)
+
+export default Logo
