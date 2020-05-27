dc14d1b86d5be25298463be3f9fd445f5dc83ab2
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-26 21:48:28 -0300

    Add new chart and fix colors

---
+++ client/src/components/charts/DegreePieChart.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react'
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Legend,
+  Cell,
+  ResponsiveContainer,
+} from 'recharts'
+
+import { COLORS } from '../../constants'
+
+const DegreePieChart: FC<Props> = ({ data }) => (
+  <div className="w-100 h-100">
+    <ResponsiveContainer>
+      <PieChart>
+        <Pie
+          data={data}
+          cx="30%"
+          cy="50%"
+          outerRadius={200}
+          dataKey="value"
+          nameKey="name"
+          blendStroke
+          label>
+          {data.map((_, index) => (
+            <Cell fill={COLORS[index]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend layout="vertical" verticalAlign="middle" align="right" />
+      </PieChart>
+    </ResponsiveContainer>
+  </div>
+)
+
+interface Props {
+  data: ChartData[]
+}
+
+export default DegreePieChart
