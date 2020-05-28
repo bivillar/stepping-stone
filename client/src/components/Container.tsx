3f3c0e77887d83c77e4df9e56b52d80e91dc1a22
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-27 23:09:46 -0300

    Add title to container

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,16 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, CSSProperties } from 'react'
 import useVisible from '../utils/hooks/useVisible'
 
-const Container: FC = ({ children }) => {
+const Container: FC<Props> = ({ style = {}, children, title }) => {
   const [isVisible, setRef] = useVisible()
 
   return (
-    <div className="homeContainer">
-      <div ref={setRef as any} className="w-100 h-100">
+    <div className="w-100 fillPage pv15 ph10" style={style}>
+      {title && (
+        <div className="header">
+          <span>{title}</span>
+        </div>
+      )}
+      <div ref={setRef as any} className="w-100 flex h-100">
         {isVisible && children}
       </div>
     </div>
   )
+}
+
+interface Props {
+  style?: CSSProperties
+  title?: string
 }
 
 export default Container
