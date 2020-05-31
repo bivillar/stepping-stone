5d9e6eba243eca55b0d6d6948c744edea4211fdb
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-31 17:20:20 -0300

    Add motives chart

--- client/src/pages/Home/Motive.tsx
+++ client/src/pages/Home/Motive.tsx
@@ -1,13 +1,26 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import YearBarChart from '../../components/charts/YearBarChart'
+import Container from '../../components/Container'
+import MotivesBarChart from '../../components/charts/MotivesBarChart'
 
-const Motives: FC<Props> = ({ formEntries }) => {
-  const [motives, setMotives] = useState<string>()
-  return <div></div>
+const Motive: FC<Props> = ({ totalizers }) => {
+  const [motives, setMotives] = useState<ChartData[]>([])
+
+  useEffect(() => {
+    setMotives([...totalizers.get('motive').values()])
+  }, [totalizers])
+
+  return (
+    <Container title="Motivos">
+      <div className="w-70" style={{ height: '40%' }}>
+        <MotivesBarChart data={motives} />
+      </div>
+    </Container>
+  )
 }
 
 interface Props {
-  formEntries: (NotInFieldFormEntry | InFieldFormEntry)[]
+  totalizers: Map<string, any>
 }
 
-export default Motives
+export default Motive
