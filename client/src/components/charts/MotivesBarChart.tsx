72a2b07f197743cd25f036d75e40001fbeac8aa8
Bianca Villar <bianca.villar@vtex.com.br>
2020-05-31 18:25:46 -0300

    Add Still In Field chart

--- client/src/components/charts/MotivesBarChart.tsx
+++ client/src/components/charts/MotivesBarChart.tsx
@@ -11,6 +11,7 @@ import {
   TooltipProps,
 } from 'recharts'
 import { COLORS } from '../../constants'
+import { getGradients } from '../../utils'
 
 const CustomTooltip = ({ active, payload, label }: TooltipProps) => {
   if (!active) return null
@@ -27,15 +28,30 @@ const MotivesBarChart: FC<Props> = ({ data }) => {
   return (
     <ResponsiveContainer>
       <BarChart layout="vertical" data={data}>
+        <defs>
+          <linearGradient
+            id="gradient"
+            spreadMethod="pad"
+            opacity={0.8}
+            x1="0"
+            y1="0"
+            x2="1"
+            y2="0">
+            <stop offset="0%" stopColor={COLORS[6]} />
+            <stop offset="68" stopColor={COLORS[3]} />
+            <stop offset="100%" stopColor={COLORS[0]} />
+          </linearGradient>
+        </defs>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis type="number" />
         <YAxis type="category" dataKey="name" width={100} />
         <Tooltip content={CustomTooltip} />
         <Legend />
         <Bar
+          animationDuration={2000}
           dataKey="value"
           name="Motivo"
-          fill={COLORS[4]}
+          fill="url(#gradient)"
           background={{ fill: COLORS[4], opacity: 0.3 }}
         />
       </BarChart>
