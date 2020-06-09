3b901371245f1259e626adb590b235ac99eb87d4
Bianca Villar <bianca.villar@vtex.com.br>
2020-06-08 21:40:16 -0300

    Add Company

---
+++ components/blocks/Company.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useState, useEffect } from 'react'
+import { TooltipProps } from 'recharts'
+
+import { getTotalizer } from '../../utils'
+import BarChart from '../charts/BarChart'
+
+const Company: FC<Props> = ({ totalizers }) => {
+  const [type, setType] = useState<any[]>([])
+  const [name, setName] = useState<any[]>([])
+
+  useEffect(() => {
+    setType(getTotalizer(totalizers, 'companyType'))
+    setName(getTotalizer(totalizers, 'companyName'))
+  }, [])
+
+  return (
+    <>
+      <div className="w-70-ns w-100" style={{ height: '40%' }}>
+        <BarChart data={type} name="Tipo de Empresa" invertGradient />
+      </div>
+    </>
+  )
+}
+
+interface Props {
+  totalizers: Totalizers
+}
+
+export default Company
