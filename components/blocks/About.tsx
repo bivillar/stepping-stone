01899167842670fdf347a93fda0930a3e51ff5ba
Bianca Villar <bianca.villar@vtex.com.br>
2020-07-05 18:58:23 -0300

    Fix mobile

--- components/blocks/About.tsx
+++ components/blocks/About.tsx
@@ -8,9 +8,9 @@ import { COLORS } from '../../utils/constants'
 
 const About: FC<Props> = ({}) => (
   <div className="w-100 ph4">
-    <div className="w-100 flex fw1">
-      <div className="w-60 pr5">
-        <h2 className="pb4  fw1">
+    <div className="w-100 flex-m dn-s">
+      <div className="w-60 ">
+        <h2 className="pb4 fw1 pr3">
           Projeto desenvolvido como Trabalho Final do curso de Engenharia da
           Computação da PUC-Rio.
         </h2>
@@ -51,11 +51,53 @@ const About: FC<Props> = ({}) => (
           </div>
         </div>
       </div>
-      <div className="w-40">
+      <div className="w-40 db-m dn-s">
         <iframe
           src="https://drive.google.com/file/d/1CYOpxhyDJJU1ySDx5RRt3kk_vh65jSZ3/preview"
           width="512"
           height="384"></iframe>
+      </div>
+    </div>
+    <div className="w-100 db-s dn-m">
+      <div className="pb4 fw1">
+        Projeto desenvolvido como Trabalho Final do curso de Engenharia da
+        Computação da PUC-Rio.
+      </div>
+      <div className="pb1 fw1">Desenvolvido por</div>
+      <div className="pb4 flex items-center">
+        <img
+          src={biancaImg}
+          style={{
+            borderRadius: '100%',
+          }}
+          width={50}
+        />
+        <div className="pl3 fw1">
+          <a
+            target="_blank"
+            style={{ color: COLORS[5] }}
+            href="https://www.linkedin.com/in/bvillar/">
+            Bianca Villar
+          </a>
+        </div>
+      </div>
+      <div className="pb1 fw1">Orientador</div>
+      <div className="pb4 flex items-center">
+        <img
+          src={marcosImg}
+          style={{
+            borderRadius: '100%',
+          }}
+          width={50}
+        />
+        <div className="pl3 fw1">
+          <a
+            target="_blank"
+            style={{ color: COLORS[5] }}
+            href="http://www-di.inf.puc-rio.br/~kalinowski">
+            Marcos Kalinowski
+          </a>
+        </div>
       </div>
     </div>
     <div className="w-100 flex items-center justify-center pt4">
